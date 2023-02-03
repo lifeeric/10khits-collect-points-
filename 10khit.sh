4be60f4e8a3cc2f704159ee1f48725f9644e4df1
EricGit <hi@ericgit.me>
2023-02-03 02:43:14 -0500

    [ADDED] complete script

---
+++ 10khit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo ""
+echo "[STARTED SURFING]"
+
+SUM=0
+for i in {1..1000}
+do
+    OUTPUT=$(source curl.sh)
+    
+    TAG=$(echo $OUTPUT | grep -o '<h4><i class="fa fa-exchange"></i>.*</h4>')
+
+    POINTS=$(echo "$TAG" |  sed -E 's/<h4><i class="fa fa-exchange"><\/i> (.*)<\/h4>/\1/');
+
+    NUM=$(echo $POINTS | grep -Eo '[0-9]+\.[0-9]+')
+
+    SCRIPT=$(echo $OUTPUT | grep -E -o '<script(.*)<\/script>');
+
+    DELAY=$(echo $SCRIPT | grep -E 'setTimeout(.*)' | grep -E -o '}, \d\d\d\d+' | grep -E -o "\d+" );
+
+    SUM=$(echo $SUM + $NUM | bc -l)
+
+    echo -ne "\033[2K\r ✅ TOTAL POINTS: $SUM | 😴 $(($DELAY / 1000))sec"
+
+    sleep $(($DELAY / 1000))
+
+done
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # 10khits-collect-points-
+
 Earn points from 10khits.com without even having to use browser!
+
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg)](https://www.youtube.com/watch?v=ryiivap9TlY)
+
+REWRITE:
+
+# How To Run
+
+To get this script running, just do the following:
+
+```
+1. Download the script and open the directory in your favorite editor.
+2. Copy the cURL from the website (as shown in the video) and paste it to the `curl.sh` file, adding the `-s` flag.
+3. Open the terminal and run `bash ./10khit.sh`.
+```
+
+Thanks a bunch!
