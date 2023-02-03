d44a85274da8d4d1911ac2b3fe23fd045cad2270
EricGit <hi@ericgit.me>
2023-02-03 08:45:44 -0500

    [REFACTOR] the bash to fix (standard_in) 1: parse error

--- 10khit.sh
+++ 10khit.sh
@@ -11,6 +11,8 @@ do
     TAG=$(echo $OUTPUT | grep -o '<h4><i class="fa fa-exchange"></i>.*</h4>')
 
     POINTS=$(echo "$TAG" |  sed -E 's/<h4><i class="fa fa-exchange"><\/i> (.*)<\/h4>/\1/');
+    
+    [[ -z $POINTS ]] && continue
 
     NUM=$(echo $POINTS | grep -Eo '[0-9]+\.[0-9]+')
 
@@ -18,9 +20,10 @@ do
 
     DELAY=$(echo $SCRIPT | grep -E 'setTimeout(.*)' | grep -E -o '}, \d\d\d\d+' | grep -E -o "\d+" );
 
+
     SUM=$(echo $SUM + $NUM | bc -l)
 
-    echo -ne "\033[2K\r ✅ TOTAL POINTS: $SUM | 😴 $(($DELAY / 1000))sec"
+    echo -ne "\033[2K\r ✅ TOTAL POINTS: $SUM | 😴 $(($DELAY / 1000)) sec"
 
     sleep $(($DELAY / 1000))
 
